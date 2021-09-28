05e8d7eeaff8512df15a679ebbf71a72e0aed997
Dom G <21165092+dgrisafe@users.noreply.github.com>
2021-09-27 21:32:58 -0700

    Add Simplified Maximum Plots

--- README.md
+++ README.md
@@ -10,10 +10,24 @@ Programming details can be referenced at [this sheet](https://docs.google.com/sp
 
 A google form was used to document strength progress. Data visualizations are created from the following .csv file on [google drive](https://docs.google.com/spreadsheets/d/1F2IPfClwYT3qm4VbRAwbtanyX0AFURBW-uvPVcoMV-8/edit?usp=sharing).
 
-## Strength Progression
+
+## Strength Progression with Warm-Ups
+
+### Overall
 
 ![](strength_timeline.png)
 
-## Strength Progression by Exercise
+### By Exercise
 
 ![](strength_timeline_facet.png)
+
+
+## Maximum Strength Progression
+
+### Max Overall
+
+![](strength_timeline_max.png)
+
+### Max by Exercise
+
+![](strength_timeline_max_facet.png)
--- startingstrength.R
+++ startingstrength.R
@@ -11,29 +11,55 @@ construct_download_url(url, format = "csv")
 
 # load data from google sheet
 df_data <- read.csv(text=gsheet2text(url,format='csv'), stringsAsFactors = FALSE)
-colnames(df_data) <- c("time", "exercise", "weight", "reps", "sets")
+colnames(df_data) <- c("date_time", "exercise", "weight", "reps", "sets")
 
 # format data types
 df_format <- df_data %>% 
   dplyr::mutate(
     exercise = factor(exercise, levels = unique(exercise)),
-    time = lubridate::mdy_hms(time)
+    date_time = lubridate::mdy_hms(date_time),
+    date = lubridate::date(date_time)
   )
 
-# create basic plot
-p_str_time <- df_format %>% 
-  ggplot(aes(x = time, y = weight, group = exercise, color = exercise)) +
-  geom_line() +
-  theme_cowplot() +
-  ylab("Weight (lbs)") +
-  xlab("Date") +
-  theme(
-    legend.position = "top",
-    legend.title = element_blank()
-  )
-p_str_time
-save_png("./strength_timeline.png", p_str_time)
 
-# plot by facets
-p_str_time_facet <- p_str_time + facet_wrap(facets = "exercise", ncol = 1, scales="free_y")
-save_png("./strength_timeline_facet.png", p_str_time_facet, dim_h = 10)
+# function to create basic plot
+plot_t_basic <- function(df){
+  df %>% 
+    ggplot(aes(x = date_time, y = weight, group = exercise, color = exercise)) +
+    geom_line() +
+    theme_cowplot() +
+    ylab("Weight (lbs)") +
+    xlab("Date") +
+    theme(
+      legend.position = "top",
+      legend.title = element_blank()
+    )
+}
+
+# function to plot by facets
+plot_t_facet <- function(p_t_basic) {
+  p_t_basic +
+    facet_wrap(facets = "exercise", ncol = 1, scales = "free_y")
+}
+
+
+# Plot w/Warm Ups
+
+  ## create basic plot
+  p_str_time <- df_format %>% plot_t_basic()
+  save_png("./strength_timeline.png", p_str_time)
+  
+  ## create facet plot
+  p_str_time_facet <- p_str_time %>% plot_t_facet()
+  save_png("./strength_timeline_facet.png", p_str_time_facet, dim_h = 10)
+  
+
+# Plot Max
+  
+  ## create basic plot
+  p_str_time_max <- df_format %>% dplyr::group_by(date, exercise) %>% top_n(n = 1, wt = weight) %>% plot_t_basic()
+  save_png("./strength_timeline_max.png", p_str_time_max)
+  
+  ## create facet plot
+  p_str_time_max_facet <- p_str_time_max %>% plot_t_facet()
+  save_png("./strength_timeline_max_facet.png", p_str_time_max_facet, dim_h = 10)
